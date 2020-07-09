39b55ba84d00a0e2434bac9e31fae89f7b8b3fe2
Whiles9799 <t.while@live.co.uk>
2020-07-09 11:42:35 +0100

    added logic for user to get by postcode

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -13,19 +13,47 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Please enter your desired stop point ID:");
-      var stopCode = Console.ReadLine();
-      Console.WriteLine(" ");
+      Console.WriteLine("Postcode or Stop point ID? (P or S)");
+      switch (Console.ReadLine())
+      {
+        case "S":
+          Console.WriteLine("Please enter your desired stop point ID:");
+          var stopCode = Console.ReadLine();
+          PrintBussesFromStopCode(stopCode);
+          break;
+        case "P":
+          Console.WriteLine("Please enter your desired postcode");
+          var postcode = Console.ReadLine();
+          foreach (var sc in getTwoClosestBusStopsToPostcode(postcode))
+          {
+            PrintBussesFromStopCode(sc);
+          }
+          break;
+        default:
+          Console.WriteLine("Please enter S or P");
+          break;
+      }
+    }
+
+    private static List<String> getTwoClosestBusStopsToPostcode(string postcode)
+    {
+      
+    }
+
+    private static void PrintBussesFromStopCode(string stopCode)
+    {
       var response = GetListOfArrivalPredictionsForStopPoint(stopCode);
-      var buses = GetNext5Buses(response);
+      var buses = GetNext5BusesFromApiResponse(response);
       Console.WriteLine($"Next 5 Buses at stop {stopCode}");
       foreach (var bus in buses)
       {
-        Console.WriteLine($"VehicleID: {bus.VehicleId}, ETA: {bus.ExpectedArrival.AddHours(1).ToString("HH:mm:ss tt")}");
+        Console.WriteLine(
+          $"VehicleID: {bus.VehicleId}, ETA: {bus.ExpectedArrival.AddHours(1).ToString("HH:mm:ss tt")}");
       }
+
       Console.WriteLine(" ");
     }
-    
+
 
     private static IRestResponse GetListOfArrivalPredictionsForStopPoint(string stopCode)
     {
@@ -36,7 +64,7 @@ namespace BusBoard.ConsoleApp
       return response;
     }
 
-    private static List<Bus> GetNext5Buses(IRestResponse response)
+    private static List<Bus> GetNext5BusesFromApiResponse(IRestResponse response)
     {
       var buses = JsonConvert.DeserializeObject<List<Bus>>(response.Content);
       buses.Sort();
