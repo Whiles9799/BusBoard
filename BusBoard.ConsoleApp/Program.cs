8e2ba23cc60e635b6bf0979b01dece23219fa53e
Whiles9799 <t.while@live.co.uk>
2020-07-09 11:42:35 +0100

    added logic for user to get by postcode

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -13,27 +13,62 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Please enter your desired stop point ID:");
-      var stopCode = Console.ReadLine();
-      Console.WriteLine(" ");
-      var buses = GetNext5BusesForStopPoint(stopCode);
+      Console.WriteLine("Postcode or Stop point ID? (P or S)");
+      switch (Console.ReadLine())
+      {
+        case "S":
+          Console.WriteLine("Please enter your desired stop point ID:");
+          var stopCode = Console.ReadLine();
+          PrintBussesFromStopCode(stopCode);
+          break;
+        case "P":
+          Console.WriteLine("Please enter your desired postcode");
+          var postcode = Console.ReadLine();
+          foreach (var sc in getTwoClosestBusStopsToPostcode(postcode))
+          {
+            PrintBussesFromStopCode(sc);
+          }
+          break;
+        default:
+          Console.WriteLine("Please enter S or P");
+          break;
+      }
+    }
+
+    private static List<String> getTwoClosestBusStopsToPostcode(string postcode)
+    {
+      
+    }
+
+    private static void PrintBussesFromStopCode(string stopCode)
+    {
+      var response = GetListOfArrivalPredictionsForStopPoint(stopCode);
+      var buses = GetNext5BusesFromApiResponse(response);
       Console.WriteLine($"Next 5 Buses at stop {stopCode}");
       foreach (var bus in buses)
       {
-        Console.WriteLine($"VehicleID: {bus.VehicleId}, ETA: {bus.ExpectedArrival.AddHours(1).ToString("HH:mm:ss tt")} ({bus.TimeToStation} seconds away)");
+        Console.WriteLine(
+          $"VehicleID: {bus.VehicleId}, ETA: {bus.ExpectedArrival.AddHours(1).ToString("HH:mm:ss tt")}");
       }
+
       Console.WriteLine(" ");
     }
 
-    private static IEnumerable<BusArrivalPrediction> GetNext5BusesForStopPoint(string stopCode)
+
+    private static IRestResponse GetListOfArrivalPredictionsForStopPoint(string stopCode)
     {
       var requestUrl = "https://api.tfl.gov.uk";
       var client = new RestClient(requestUrl);
-      var request = new RestRequest($"StopPoint/{stopCode}/Arrivals");
-      var buses = client.Execute<List<BusArrivalPrediction>>(request).Data;
-      IEnumerable<BusArrivalPrediction> orderedBuses = buses.OrderBy(bus => bus.ExpectedArrival).Take(5);
-      return orderedBuses;
+      var request = new RestRequest($"StopPoint/{stopCode}/Arrivals", DataFormat.Json);
+      var response = client.Execute(request);
+      return response;
     }
-    
+
+    private static List<Bus> GetNext5BusesFromApiResponse(IRestResponse response)
+    {
+      var buses = JsonConvert.DeserializeObject<List<Bus>>(response.Content);
+      buses.Sort();
+      return buses.GetRange(0, 5);
+    }
   }
 }
