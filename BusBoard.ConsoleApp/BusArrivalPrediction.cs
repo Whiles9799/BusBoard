3c8191591c78bc715e1b6aedfc3434e34d92069f
Whiles9799 <t.while@live.co.uk>
2020-07-09 13:49:51 +0100

    pull request recommendations implemented bus class renamed

---
+++ BusBoard.ConsoleApp/BusArrivalPrediction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BusBoard.ConsoleApp
+{
+    public class BusArrivalPrediction
+    {
+       public string VehicleId { get; set; }
+       public string Bearing { get; set; }
+       public DateTime ExpectedArrival { get; set; }
+       public int TimeToStation { get; set; }
+       
+    }
+    
+}
