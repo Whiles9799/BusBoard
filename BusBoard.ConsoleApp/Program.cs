7f46c2f388c5e312ad89f40dceff163cee416683
Whiles9799 <t.while@live.co.uk>
2020-07-09 13:49:51 +0100

    pull request recommendations implemented bus class renamed

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -13,51 +13,27 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
-      TflApi api = new TflApi();
-      PromptUserAndGetInputFromConsole(api);
-    }
-
-    private static void PromptUserAndGetInputFromConsole(TflApi api)
-    {
-      Console.WriteLine("Postcode or Stop point ID? (P or S)");
-      switch (Console.ReadLine().ToUpper())
-      {
-        case "S":
-          Console.WriteLine("Please enter your desired stop point ID:");
-          var stopCode = Console.ReadLine();
-          Console.WriteLine(" ");
-          PrintBusesFromStopCode(stopCode, "", api);
-          break;
-        case "P":
-          Console.WriteLine("Please enter your desired postcode");
-          var postcode = Console.ReadLine();
-          Console.WriteLine(" ");
-          foreach (var sc in api.GetTwoClosestBusStopsToPostcode(postcode))
-          {
-            PrintBusesFromStopCode(sc.NaptanId, sc.CommonName, api);
-          }
-
-          break;
-        default:
-          Console.WriteLine("Please enter S or P");
-          break;
-      }
-    }
-
-    private static void PrintBusesFromStopCode(string stopCode, string commonName, TflApi api)
-    {
-      var buses = api.GetListOfArrivalPredictionsForStopPoint(stopCode);
-      Console.WriteLine($"Next 5 Buses at stop {stopCode} - {commonName}");
+      Console.WriteLine("Please enter your desired stop point ID:");
+      var stopCode = Console.ReadLine();
+      Console.WriteLine(" ");
+      var buses = GetNext5BusesForStopPoint(stopCode);
+      Console.WriteLine($"Next 5 Buses at stop {stopCode}");
       foreach (var bus in buses)
       {
-        Console.WriteLine(
-          $"VehicleID: {bus.VehicleId}, ETA: {bus.ExpectedArrival.AddHours(1).ToString("HH:mm:ss tt")}");
+        Console.WriteLine($"VehicleID: {bus.VehicleId}, ETA: {bus.ExpectedArrival.AddHours(1).ToString("HH:mm:ss tt")} ({bus.TimeToStation} seconds away)");
       }
-
       Console.WriteLine(" ");
     }
-    
-    
+
+    private static IEnumerable<BusArrivalPrediction> GetNext5BusesForStopPoint(string stopCode)
+    {
+      var requestUrl = "https://api.tfl.gov.uk";
+      var client = new RestClient(requestUrl);
+      var request = new RestRequest($"StopPoint/{stopCode}/Arrivals");
+      var buses = client.Execute<List<BusArrivalPrediction>>(request).Data;
+      IEnumerable<BusArrivalPrediction> orderedBuses = buses.OrderBy(bus => bus.ExpectedArrival).Take(5);
+      return orderedBuses;
+    }
     
   }
 }
