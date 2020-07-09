8e3c661f58e11d8d683401b74a0431d5575e9d26
Whiles9799 <t.while@live.co.uk>
2020-07-09 13:49:51 +0100

    pull request recommendations implemented bus class renamed

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -16,31 +16,24 @@ namespace BusBoard.ConsoleApp
       Console.WriteLine("Please enter your desired stop point ID:");
       var stopCode = Console.ReadLine();
       Console.WriteLine(" ");
-      var response = GetListOfArrivalPredictionsForStopPoint(stopCode);
-      var buses = GetNext5Buses(response);
+      var buses = GetNext5BusesForStopPoint(stopCode);
       Console.WriteLine($"Next 5 Buses at stop {stopCode}");
       foreach (var bus in buses)
       {
-        Console.WriteLine($"VehicleID: {bus.VehicleId}, ETA: {bus.ExpectedArrival.AddHours(1).ToString("HH:mm:ss tt")}");
+        Console.WriteLine($"VehicleID: {bus.VehicleId}, ETA: {bus.ExpectedArrival.AddHours(1).ToString("HH:mm:ss tt")} ({bus.TimeToStation} seconds away)");
       }
       Console.WriteLine(" ");
     }
-    
 
-    private static IRestResponse GetListOfArrivalPredictionsForStopPoint(string stopCode)
+    private static IEnumerable<BusArrivalPrediction> GetNext5BusesForStopPoint(string stopCode)
     {
       var requestUrl = "https://api.tfl.gov.uk";
       var client = new RestClient(requestUrl);
-      var request = new RestRequest($"StopPoint/{stopCode}/Arrivals", DataFormat.Json);
-      var response = client.Execute(request);
-      return response;
+      var request = new RestRequest($"StopPoint/{stopCode}/Arrivals");
+      var buses = client.Execute<List<BusArrivalPrediction>>(request).Data;
+      IEnumerable<BusArrivalPrediction> orderedBuses = buses.OrderBy(bus => bus.ExpectedArrival).Take(5);
+      return orderedBuses;
     }
-
-    private static List<Bus> GetNext5Buses(IRestResponse response)
-    {
-      var buses = JsonConvert.DeserializeObject<List<Bus>>(response.Content);
-      buses.Sort();
-      return buses.GetRange(0, 5);
-    }
+    
   }
 }
