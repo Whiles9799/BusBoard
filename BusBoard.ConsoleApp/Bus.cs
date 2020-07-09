c1846762e224cbdb5d80d1f6b5d5e38a0524c6aa
Whiles9799 <t.while@live.co.uk>
2020-07-09 11:22:20 +0100

    Improved method names Takes user input for stop code

--- BusBoard.ConsoleApp/Bus.cs
+++ BusBoard.ConsoleApp/Bus.cs
@@ -1,7 +1,20 @@
-﻿namespace BusBoard.ConsoleApp
+﻿using System;
+
+namespace BusBoard.ConsoleApp
 {
-    public class Bus
+    public class Bus : IComparable
     {
-        
+       public string VehicleId { get; set; }
+       public string Bearing { get; set; }
+       public DateTime ExpectedArrival { get; set; }
+       public int CompareTo(object obj)
+       {
+           if (obj == null) return 1;
+           Bus other = obj as Bus;
+           if (other != null) return this.ExpectedArrival.CompareTo(other.ExpectedArrival);
+           throw new ArgumentException("Object is not a Bus");
+
+       }
     }
+    
 }
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BusBoard.ConsoleApp
 {
@@ -10,6 +13,34 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
+      Console.WriteLine("Please enter your desired stop point ID:");
+      var stopCode = Console.ReadLine();
+      Console.WriteLine(" ");
+      var response = GetListOfArrivalPredictionsForStopPoint(stopCode);
+      var buses = GetNext5Buses(response);
+      Console.WriteLine($"Next 5 Buses at stop {stopCode}");
+      foreach (var bus in buses)
+      {
+        Console.WriteLine($"VehicleID: {bus.VehicleId}, ETA: {bus.ExpectedArrival.AddHours(1).ToString("HH:mm:ss tt")}");
+      }
+      Console.WriteLine(" ");
+    }
+    
+
+    private static IRestResponse GetListOfArrivalPredictionsForStopPoint(string stopCode)
+    {
+      var requestUrl = "https://api.tfl.gov.uk";
+      var client = new RestClient(requestUrl);
+      var request = new RestRequest($"StopPoint/{stopCode}/Arrivals", DataFormat.Json);
+      var response = client.Execute(request);
+      return response;
+    }
+
+    private static List<Bus> GetNext5Buses(IRestResponse response)
+    {
+      var buses = JsonConvert.DeserializeObject<List<Bus>>(response.Content);
+      buses.Sort();
+      return buses.GetRange(0, 5);
     }
   }
 }
